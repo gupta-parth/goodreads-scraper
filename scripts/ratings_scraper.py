5d78766e87427c016157326d768ae96ca8d6f81d
Parth Gupta <parthgupta023@gmail.com>
2022-05-19 19:37:09 -0400

    Scraped first 10 rows to assess data

--- scripts/ratings_scraper.py
+++ scripts/ratings_scraper.py
@@ -1,6 +1,7 @@
 import csv
 import re
 from bs4 import BeautifulSoup
+from matplotlib.pyplot import text
 import requests
 from sympy import content
 
@@ -16,32 +17,38 @@ with open('data/txtlab_CONLIT_META_2022.csv', 'r', encoding='utf8', newline='') 
     reader = csv.reader(f, delimiter='\t')      # File is delimited with tab
     line_count = 0
     for row in reader:
-        author_first = row[7]
-        author_last = row[6]
-
-        # The name doesn't have spaces, each new word starts with a capital
-        name = row[8]
-        name_list = [word for word in re.split('([A-Z][^A-Z]*)', name) if word]
-
-        # Constructing the search url
-        search_book_url = search_url + author_first + '+' + author_last
-        for word in name_list:
-            search_book_url += '+' + word
-
-        # Set up soup object
-        page = requests.get(search_book_url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        # Get all the book results
-        main_content_div = soup.find('div', class_='mainContent')
-        main_content_float = main_content_div.find(
-            'div', class_='mainContentFloat')
-        left_container = main_content_float.find('div', class_='leftContainer')
-        book_results = soup.find_all('tr')
-
         if line_count != 0:
+            author_first = row[7]
+            author_last = row[6]
+
+            # The name doesn't have spaces, each new word starts with a capital
+            name = row[8]
+            name_list = [word for word in re.split(
+                '([A-Z][^A-Z]*)', name) if word]
+
+            # Constructing the search url
+            search_book_url = search_url + author_first + '+' + author_last
+            for word in name_list:
+                search_book_url += '+' + word
+
+            # Set up soup object
+            page = requests.get(search_book_url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            # Get all the book results
+            main_content_div = soup.find('div', class_='mainContent')
+            main_content_float = main_content_div.find(
+                'div', class_='mainContentFloat')
+            left_container = main_content_float.find(
+                'div', class_='leftContainer')
+            book_results = soup.find_all('tr')
             print(
-                f'Author: {author_first} {author_last}, Book: {str(name_list)}, Search: {search_book_url}')
+                f'Line: {line_count + 1}, Author: {author_first} {author_last}, Book: {str(name_list)}, Search: {search_book_url}')
+            for book in book_results:
+                rating_text = book.find('span', class_='minirating').text
+                text_list = rating_text.split()
+                print(f"Avg: {text_list[0]}, Number: {text_list[4]}")
+                print('----------------')
         line_count += 1
-        if line_count == 2:
+        if line_count == 10:
             break
